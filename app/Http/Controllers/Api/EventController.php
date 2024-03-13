5dfbc5422fc2e81e91bcf318a7dc74b2844d4019
Adam Wlazlo <adam.wlazlo@gmail.com>
2024-03-13 20:50:55 +0100

    authorisation (bearer token)

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -14,6 +14,11 @@ class EventController extends Controller
 
     private array $relations = ['user', 'attendees', 'attendees.user'];
 
+    public function __construct()
+    {
+        $this->middleware('auth:sanctum')->except('index', 'show');
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -36,7 +41,7 @@ class EventController extends Controller
             'end_time' => 'required|date|after:start_time',
         ]);
 
-        $validatedData['user_id'] = 1;
+        $validatedData['user_id'] = $request->user()->id;
 
         $event = Event::create($validatedData);
 
